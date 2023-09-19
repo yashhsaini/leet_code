886ceb40e558867409ba339a0c2fe31620ca0764
yogesh <124228964+yashhsaini@users.noreply.github.com>
2023-09-19 12:04:36 +0530

    Update anagram.cpp

--- anagram.cpp
+++ anagram.cpp
@@ -9,3 +9,24 @@ bool isAnagram(string s, string t) {
     {return false;}}
     return true;}
 };
+
+
+---X array method X-----
+    class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    int count[26] = {0};
+        for (char x : s) {
+            count[x - 'a']++;
+        }
+        for (char x : t) {
+            count[x - 'a']--;
+        }
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
